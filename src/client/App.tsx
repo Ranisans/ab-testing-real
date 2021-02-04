fe52a5c270d4822c6e03530f9547b54cde3ff8aa
ranisans <ranisans@yahoo.com>
2021-02-04 13:09:47 +0300

    add load users data from server add creating new user

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { StatusCodes } from "http-status-codes";
 
 import Table from "./components/Table";
-import { IRowData } from "./types";
 import { DateInput } from "./components/Input";
+import Button from "./components/Button";
+import ErrorWindow from "./components/ErrorWindow";
+import { IRowData } from "./types";
 
 import "./App.scss";
-import Button from "./components/Button";
 
 enum EDate {
   REGISTRATION,
@@ -13,18 +15,41 @@ enum EDate {
 }
 
 interface INewRecord {
-  registrationDate: Date | null;
-  lastActivityDate: Date | null;
+  registrationDate: Date;
+  lastActivityDate: Date;
 }
 
 const newUserInitialDate: INewRecord = {
-  registrationDate: null,
-  lastActivityDate: null,
+  registrationDate: new Date(),
+  lastActivityDate: new Date(),
 };
 
 const App: React.FC = () => {
   const [userData, setUserData] = useState<INewRecord>(newUserInitialDate);
   const [rollingRetention, setRollingRetention] = useState(0);
+  const [tableData, setTableData] = useState<IRowData[]>([]);
+  const [showError, setShowError] = useState(false);
+  const [errorText, setErrorText] = useState("");
+
+  const handleError = () => {
+    setShowError(!showError);
+  };
+
+  const updateData = async () => {
+    const result = await fetch("/user");
+    if (result.status === StatusCodes.OK) {
+      const newData = await result.json();
+      setTableData(newData.data);
+    } else {
+      setErrorText("Server Error!");
+      handleError();
+    }
+  };
+
+  useEffect(() => {
+    updateData();
+  }, []);
+
   const handleChange = (key: EDate) => (date: Date) => {
     if (key === EDate.REGISTRATION) {
       setUserData({ ...userData, registrationDate: date });
@@ -33,21 +58,40 @@ const App: React.FC = () => {
     }
   };
 
-  const handleSetUser = () => {
-    console.log("OK");
+  const handleSetUser = async () => {
+    if (userData.registrationDate > userData.lastActivityDate) {
+      setErrorText("Wrong User date!");
+      handleError();
+    } else {
+      const result = await fetch("/user", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(userData),
+      });
+      if (result.status === StatusCodes.OK) {
+        updateData();
+        setUserData(newUserInitialDate);
+        setErrorText("Done!");
+        handleError();
+      } else {
+        setErrorText(result.statusText);
+        handleError();
+      }
+    }
   };
 
   const handleCalculate = () => {
     setRollingRetention(12);
   };
 
-  const data: IRowData[] = [];
-
   return (
     <div className="app">
       <h1>AB Test Real</h1>
       <main>
-        <Table data={data} className="app-table" />
+        <Table data={tableData} className="app-table" />
         <div className="app_block">
           <div className="app-input_block">
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
@@ -76,6 +120,11 @@ const App: React.FC = () => {
           />
         </div>
       </main>
+      <ErrorWindow
+        message={errorText}
+        isShown={showError}
+        callback={handleError}
+      />
     </div>
   );
 };
