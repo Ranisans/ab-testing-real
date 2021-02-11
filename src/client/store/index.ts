9f7b3c926cfa0a24052fe0b2ba6146070b7a28b9
ranisans <ranisans@yahoo.com>
2021-02-11 11:04:21 +0300

    fix redux error when store Date

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import rootReducer from "./rootReducer";
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 if (process.env.NODE_ENV === "development" && module.hot) {
